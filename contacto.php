65eaa9caac83df151b8d4b14f3a63ec0fcc0a155
091910334-Roselino <diaz.sanchez.rose@gmail.com>
2022-03-14 23:15:25 -0600

    Primer Screenshot de lProyecto GitMundo "Roselino"

---
+++ contacto.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Fredoka&display=swap" rel="stylesheet">
+    <title>Contacto</title>
+</head>
+<body>
+    <div class="contacto">
+    <p>Correo: diaz.sanchez.rose@gmail.com</p>
+    <p>Telefono: 9191541503</p>
+    <p>Facebook: Rose diaz</p>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Fredoka&display=swap" rel="stylesheet">
+    <title>Inicio</title>
+</head>
+<body class="inicio">
+    <h1>En nuestro proyecto integrador, haremos una aplicacion web en la que podamos controlar algunas funciones basicas de una casa, como son las luces puertas, ventanas, alarmas, entre otras cosas</h1>
+</body>
+</html>
